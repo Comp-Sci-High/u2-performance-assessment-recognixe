f18aa8e2907c6e20e2e8cfc14aae8c66c4706849
recognixe <angel.felix25@compscihigh.org>
2024-11-19 14:36:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+let CatFurl = "https://meowfacts.herokuapp.com/"
+
+
+
+let input = prompt("I'm an IA that shows you cats and a fact about them wht type of could would you like to see? ")
+async function Catfunction(){
+const response = await fetch(CatFurl);
+const data = await response.json();
+console.log(data.data[0])
+}
+
+Catfunction() 
+
+const prompt = require(‘prompt-sync’)()
+
+
+
+let GBTurl = "https://api.openai.com/v1/images/generations"
+
+async function createPlaylist(playlistData) {
+  
+    const options = {
+  method:"POST",
+  headers:{
+        Authorization:"Bearer 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",
+    "Content-Type": "application/json",
+  },
+  body:JSON.stringify(playlistData)
+}
+
+  const response = await fetch(GBTurl,options);  
+  const data = await response.json();
+  console.log(data)
+  return data; 
+  }
+
+  createPlaylist({
+    prompt: "cat"  })
+ 
 
